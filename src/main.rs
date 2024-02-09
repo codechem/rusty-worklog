ceb7c70fa095a18dfe488c1c523803a367801f5e
Tomislav <tomislavignjatov@rocketmail.com>
2024-02-09 15:05:55 +0100

    introduce filtering by username and date

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use chrono::NaiveDate;
 use core::panic;
-use std::process::Command;
+use std::{process::Command, str::FromStr};
 
 #[derive(Debug)]
 struct CommandOutput {
@@ -41,7 +41,7 @@ impl CommandOutput {
 fn get_git_logs(file_path: String) -> Vec<CommandOutput> {
     let output = Command::new(String::from("git"))
         .current_dir(file_path)
-        .args(["log", "-2", "--pretty='%as,%f,%an'"])
+        .args(["log", "--pretty='%as,%s,%an'"])
         .output()
         .expect("failed to execute process");
 
@@ -58,8 +58,38 @@ fn get_git_logs(file_path: String) -> Vec<CommandOutput> {
     commands_output
 }
 
+fn filter_by_date(command_logs: Vec<CommandOutput>, date: NaiveDate) -> Vec<CommandOutput> {
+    command_logs
+        .into_iter()
+        .filter(|x| x.date >= date)
+        .collect()
+}
+
+fn filter_by_user(command_logs: Vec<CommandOutput>, user_name: &String) -> Vec<CommandOutput> {
+    command_logs
+        .into_iter()
+        .filter(|x| &x.author == user_name)
+        .collect()
+}
+
+fn filter_logs(
+    command_logs: Vec<CommandOutput>,
+    user_name: &String,
+    date: NaiveDate,
+) -> Vec<CommandOutput> {
+    let date_filter = filter_by_date(command_logs, date);
+    filter_by_user(date_filter, user_name)
+}
+
 fn main() {
     let example_file_path = String::from("/Users/tomislav/Projects/xarvio/hrl-config");
     let outputs = get_git_logs(example_file_path);
-    println!("{:?}", outputs)
+    let date_result = NaiveDate::from_str("2024-02-01");
+    let date = match date_result {
+        Ok(date_result) => date_result,
+        Err(..) => panic!("Error with the date"),
+    };
+    let user = String::from("IgnjatT");
+    let logs = filter_logs(outputs, &user, date);
+    println!("{:?}", logs)
 }
