6e898d3f249971ebd784cc3f651b2104b7d80329
Tomislav <tomislavignjatov@rocketmail.com>
2024-02-12 22:44:28 +0100

    implemented use of config.json and ability to parse multiple repositories

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use chrono::NaiveDate;
 use core::panic;
 use itertools::Itertools;
 use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
 use std::{process::Command, str::FromStr};
 
+use serde::Deserialize;
 mod models;
 use crate::models::{CommandOutput, IssueLog};
 
@@ -72,22 +76,65 @@ fn write_to_file(
     Ok(())
 }
 
-fn main() {
-    let example_file_path = String::from("/Users/tomislav/Projects/xarvio/hrl-config");
-    let outputs = get_git_logs(example_file_path);
-    let date_result = NaiveDate::from_str("2023-11-01");
-    let date = match date_result {
-        Ok(date_result) => date_result,
-        Err(..) => panic!("Error with the date"),
-    };
-    let user = String::from("IgnjatT");
-    let result_file_path = String::from("worklog.csv");
+fn get_logs_from_project(file_path: String, user: &String, date: &NaiveDate) -> Vec<IssueLog> {
+    let outputs = get_git_logs(file_path);
     let mut logs = filter_logs(outputs, &user, &date);
     logs.sort_by_key(|a| extract_issue_from_msg(&a));
 
     let mut issue_logs = get_issue_logs(logs);
     issue_logs.sort_by_key(|a| a.date);
-    if let Err(err) = write_to_file(issue_logs, &result_file_path) {
-        println!("error running example: {}", err);
+    issue_logs
+}
+
+#[derive(Deserialize, Debug)]
+struct User {
+    author: String,
+    projectName: String,
+    repositories: Vec<String>,
+    date: String,
+}
+fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<User, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let u = serde_json::from_reader(reader)?;
+
+    Ok(u)
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let config_filepath = match args.get(1) {
+        Some(v) => v,
+        None => panic!("No config file provided"),
+    };
+    let config = read_user_from_file(config_filepath);
+    let content = match config {
+        Ok(e) => e,
+        Err(e) => panic!("Error reading config file: {}", e),
+    };
+    println!("Found config: {:?}", content);
+    let test_project_1 = String::from("");
+    let test_project_2 = String::from("");
+    let date_result = NaiveDate::from_str("2023-11-01");
+    let date = match date_result {
+        Ok(date_result) => date_result,
+        Err(..) => panic!("Error with the date"),
+    };
+    let user = String::from("");
+    let result_file_path = String::from("worklog.csv");
+    let logs1 = get_logs_from_project(test_project_1, &user, &date);
+    let logs2 = get_logs_from_project(test_project_2, &user, &date);
+    let mut final_logs = logs1
+        .into_iter()
+        .chain(logs2.into_iter())
+        .collect::<Vec<IssueLog>>();
+    final_logs.sort_by_key(|a| (a.date, a.issue.clone()));
+    final_logs.dedup_by_key(|a| (a.date, a.issue.clone()));
+    final_logs.sort_by_key(|a| a.date);
+    let result = write_to_file(final_logs, &result_file_path);
+    match result {
+        Ok(_) => println!("Worklog written to {}", &result_file_path),
+        Err(e) => println!("Error writing to file: {}", e),
     }
 }
